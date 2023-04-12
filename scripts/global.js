5e86d21ef8d39620c3fb4720b75f206f51e18ecb
ammac <alexandremmac@gmail.com>
2023-04-12 20:12:49 -0300

    add pre loading page and numbers animation

---
+++ scripts/global.js
@@ -0,0 +1,9 @@
+function loadingPage() {
+    let preloadElement = document.getElementById("preload");
+    let loadedElement = document.getElementById("loaded");
+
+    // setTimeout(() => {
+        preloadElement.style.display = "none";
+        loadedElement.style.display = "block";
+    // }, 5000);
+}
---
+++ scripts/home.js
@@ -0,0 +1,26 @@
+let metricNumbers = document.querySelectorAll(".metric-number");
+
+window.addEventListener('scroll', metricAnimation);
+
+function metricAnimation() {
+    metricNumbers.forEach((valueDisplay) => {
+        let viewportHeight = document.documentElement.clientHeight;
+        let displayRect = valueDisplay.getBoundingClientRect();
+        
+        if (displayRect.top < viewportHeight && valueDisplay.innerHTML === "000") {
+            let startValue = 0;
+            let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+        
+            // let duration = Math.floor(interval / endValue);
+            let counter = setInterval(() => {
+                startValue += 1;
+                valueDisplay.textContent = startValue;
+                if (startValue == endValue) {
+                    clearInterval(counter);
+                }
+            }, 4);
+        } else {
+            return;
+        }
+    });
+}
